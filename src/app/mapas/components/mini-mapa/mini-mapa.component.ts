a2919d6d5553e2b10194cf75c8dcc7925cb88c1d
Daniel Martinez <dmartmar@mercadona.es>
2022-07-26 15:57:28 +0200

    feat: Creado minimapa

--- src/app/mapas/components/mini-mapa/mini-mapa.component.ts
+++ src/app/mapas/components/mini-mapa/mini-mapa.component.ts
@@ -1,16 +1,40 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, Input, ViewChild } from '@angular/core';
+import * as mapboxgl from 'mapbox-gl';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-mini-mapa',
   templateUrl: './mini-mapa.component.html',
   styles: [
+    `
+      div {
+        width: 100%;
+        height: 150px;
+        margin: 0;
+      }
+    `
   ]
 })
-export class MiniMapaComponent implements OnInit {
+export class MiniMapaComponent implements AfterViewInit {
+
+  @Input() longitudLatitud: [number, number] = [0, 0];
+  @ViewChild('mapa') divMapa!: ElementRef;
 
   constructor() { }
 
-  ngOnInit(): void {
+  ngAfterViewInit(): void {
+
+    const mapa = new mapboxgl.Map({
+      container: this.divMapa.nativeElement,
+      style: 'mapbox://styles/mapbox/streets-v11',
+      center: this.longitudLatitud,
+      zoom: 15,
+      interactive: false
+    });
+
+    new mapboxgl.Marker()
+        .setLngLat(this.longitudLatitud)
+        .addTo(mapa);
   }
 
 }
